7f39e35b1635a3c8c7ac664d10b4e2bd23a5f579
Marcelo Belo de Souza <beloadm43@gmail.com>
2024-07-16 16:57:47 -0300

    atual
    criação do menu hamburguer

--- script/script.js
+++ script/script.js
@@ -1,2 +1,9 @@
+const btnMoBile = document.getElementById('btn-mobile');
 
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+btnMoBile.addEventListener('click', toggleMenu);
 
