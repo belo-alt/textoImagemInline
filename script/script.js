1a1a038626bd7021725a9beae03f7d2b00cc04af
Marcelo Belo de Souza <beloadm43@gmail.com>
2024-07-17 14:38:40 -0300

    Talual
    Implementação do menu amburguinho

--- script/script.js
+++ script/script.js
@@ -1,9 +1,25 @@
-const btnMoBile = document.getElementById('btn-mobile');
+document.addEventListener("DOMContentLoaded", function() {
+    var header = document.getElementById("header");
+    var nav = document.getElementById("nav");
+    var menu = document.getElementById("menu");
 
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-}
+    // Função para mostrar o menu hambúrguer ao rolar
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            header.style.top = "0";
+        } else {
+            header.style.top = "-60px";
+        }
+    });
 
-btnMoBile.addEventListener('click', toggleMenu);
-
+    // Função para alternar a visibilidade do menu de navegação
+    menu.addEventListener("click", function() {
+        if (nav.classList.contains("hidden")) {
+            nav.classList.remove("hidden");
+            nav.classList.add("visible");
+        } else {
+            nav.classList.remove("visible");
+            nav.classList.add("hidden");
+        }
+    });
+});
